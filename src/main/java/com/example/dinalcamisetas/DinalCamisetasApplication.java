d0d3b8c22764a523c7e9db119f3e6e44c1f777ae
Nicolas Salazar <josalaza@bancolombia.com.co>
2023-12-17 23:48:51 -0500

    v1

---
+++ src/main/java/com/example/dinalcamisetas/DinalCamisetasApplication.java
@@ -0,0 +1,13 @@
+package com.example.dinalcamisetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinalCamisetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinalCamisetasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dinalcamisetas/DinalCamisetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dinalcamisetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinalCamisetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
